47b22bf9fede16dfa0af89f68a555fa85a079797
siddu <siddunaroju@gmail.com>
2022-10-13 21:47:37 +0530

    Revert "Pushing to branch"
    This reverts commit 70943253e50cd77d44f4169386e150c800a33cd3.

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
 import InputOption from "./InputOption";
@@ -6,80 +6,15 @@ import ImageIcon from "@mui/icons-material/Image";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
-import Post from "./Post";
-import db from "./firebase";
-import {
-  collection,
-  addDoc,
-  doc,
-  onSnapshot,
-  serverTimestamp,
-  query,
-  orderBy,
-} from "firebase/firestore";
 function Feed() {
-  const [input, setInput] = useState("");
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
-    onSnapshot(q, (snapshot) => {
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
-      );
-    });
-    // db.collection("posts").onSnapshot((snapshot) => {
-    //   setPosts(
-    //     snapshot.docs.map((doc) => ({
-    //       id: doc.id,
-    //       data: doc.data(),
-    //     }))
-    //   );
-    // });
-    // snapshot.docs.map((doc) => ({
-    //   id: doc.id,
-    //   data: doc.data(),
-    // }))
-    //   let books = [];
-    //   snapshot.docs.forEach((doc) => {
-    //     books.push({ ...doc.data(), id: doc.id });
-    //   });
-    //   console.log(books);
-  }, []);
-  const sendPost = (e) => {
-    e.preventDefault();
-    // db.collection("posts").add({
-    //   name: "Siddu",
-    //   description: "just for test",
-    //   message: "",
-    // });
-    addDoc(collection(db, "posts"), {
-      name: "Siddu",
-      description: "just for test",
-      message: input,
-      photoUrl: "",
-      timestamp: serverTimestamp(),
-    });
-    setInput("");
-  };
   return (
     <div className="feed">
-      {/* feed input goes here */}
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon></CreateIcon>
           <form action="">
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              type="text"
-            />
-            <button onClick={sendPost} type="submit">
-              Send
-            </button>
+            <input type="text" />
+            <button type="submit">Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -93,20 +28,6 @@ function Feed() {
           />
         </div>
       </div>
-      {/* Posts feed goes here */}
-      {posts.map((post) => (
-        <Post
-          name={post.name}
-          description={post.description}
-          message={post.message}
-        />
-      ))}
-
-      {/* <Post
-        name="Siddu Naroju"
-        description="Some test desc.."
-        message="This is working"
-      /> */}
     </div>
   );
 }
